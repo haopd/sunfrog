5d417120c4a82643cab37baa3888cdf5f2e3f360
Pham Tien Dat <datpt.11.1.1992@gmail.com>
2016-07-09 03:28:31 +0700

    alohaden

--- app/route/__init__.py
+++ app/route/__init__.py
@@ -5,6 +5,8 @@ import app
 import formencode
 from formencode import validators
 from controller import db
+from google.appengine.api import search
+from google.appengine.ext import ndb
 
 __author__ = 'datpt'
 _logger = logging.getLogger(__name__)
@@ -19,7 +21,22 @@ class AddUrlForm(formencode.Schema):
 
 class ViewUrlHandler(app.BaseRequestHandler):
     def get(self):
-        list_url = db.Url.query().order(-db.Url.time_created).fetch(limit=20)
+        keyword_search = self.request.GET.get('search')
+        index = search.Index(name='url_fulltextsearch')
+        query_string = 'url_input_search = %s OR url_output_search = %s' % \
+                       (keyword_search, keyword_search)
+        search_results = index.search(search.Query(
+            query_string=query_string,
+            options=search.QueryOptions(
+                limit=10
+            )
+        ))
+        list_ids = []
+        list_url = []
+        for result in search_results:
+            list_ids.append(result.doc_id)
+        if list_ids:
+            list_url = ndb.get_multi([ndb.Key(db.Url, int(k)) for k in list_ids])
         return self.render_template('frontend/view.j2', list_url=list_url)
 
 
@@ -60,19 +77,42 @@ class DetailUrlHandler(app.BaseRequestHandler):
 class EditUrlHandler(app.BaseRequestHandler):
     def get(self, url_id):
         url = db.Url.get_by_id(int(url_id))
-        self.render_template('/frontend/detail.j2', url=url)
+        self.render_template('/frontend/edit.j2', url=url, form_data=None,
+                             form_errors=None)
+
+    def post(self, url_id):
+        url = db.Url.get_by_id(int(url_id))
+        form_validate = AddUrlForm()
+        form_errors = {}
+        try:
+            data = form_validate.to_python(self.request.POST)
+            obj = db.Url.get_by_id(int(url_id))
+            obj.url_input = data.get('url_input')
+            obj.url_output = data.get('url_output')
+            obj.status = True
+            obj.put()
+            self.session.add_flash('Success')
+            return self.redirect_to('url/view', url_id=url_id)
+        except formencode.Invalid as e:
+            form_errors = e.error_dict
+        self.render_template('frontend/add.j2',
+                             form_data=self.request.POST.mixed(),
+                             form_errors=form_errors, url=url)
 
 
 class DeleteUrlHandler(app.BaseRequestHandler):
     def get(self, url_id):
         url = db.Url.get_by_id(int(url_id))
-        self.render_template('/frontend/detail.j2', url=url)
+        url.key.delete()
+        return self.redirect_to('url')
 
 webapp2_routes = [
     webapp2.Route('/', handler=MainHandler, name='home'),
     webapp2.Route(r'/url', name='url', handler=ViewUrlHandler),
-    webapp2.Route(r'/url/<url_id>', name='url/view', handler=DetailUrlHandler),
-    webapp2.Route(r'/url/<url_id>', name='url/edit', handler=EditUrlHandler),
-    webapp2.Route(r'/url/<url_id>', name='url/delete', handler=DeleteUrlHandler),
     webapp2.Route(r'/url/add', name='url/add', handler=AddUrlHandler),
+    webapp2.Route(r'/url/edit/<url_id>', name='url/edit',
+                  handler=EditUrlHandler),
+    webapp2.Route(r'/url/delete/<url_id>', name='url/delete',
+                  handler=DeleteUrlHandler),
+    webapp2.Route(r'/url/<url_id>', name='url/view', handler=DetailUrlHandler),
 ]
--- controller/db.py
+++ controller/db.py
@@ -1,14 +1,46 @@
 # -*- coding: utf-8 -*-
 import logging
 from google.appengine.ext import ndb
+from google.appengine.api import search
 
 __author__ = 'datpt'
 _logger = logging.getLogger(__name__)
 
 
 class Url(ndb.Model):
+    def _post_put_hook(self, future):
+        rs = super(Url, self)._post_put_hook(future)
+        item_obj = Url.get_by_id(((future.get_result().id())))
+        if item_obj:
+            url_input = ','.join(tokenize_autocomplete(item_obj.url_input))
+            url_output = ','.join(tokenize_autocomplete(item_obj._input))
+            my_document = search.Document(
+                doc_id=str(future.get_result().id()),
+                fields=[
+                    search.TextField(name='url_input_search',
+                                     value=url_input),
+                    search.TextField(name='url_output_search',
+                                     value=url_output)
+                ])
+            index = search.Index(name="url_fulltextsearch")
+            index.put(my_document)
+        return rs
+
     url_input = ndb.StringProperty(required=True)
     url_output = ndb.StringProperty(required=True)
     status = ndb.BooleanProperty(default=True)
     time_created = ndb.DateTimeProperty(auto_now_add=True)
-    time_modify =ndb.DateTimeProperty(auto_now=True)
+    time_modify = ndb.DateTimeProperty(auto_now=True)
+
+
+def tokenize_autocomplete(phrase):
+    a = []
+    for word in phrase.split():
+        j = 5
+        while True:
+            for i in range(len(word) - j + 1):
+                a.append(word[i:i + j])
+            if j == len(word):
+                break
+            j += 1
+    return a
