180c896087e0d8a2ce8fe2c08321e6bf0dde79ff
haopd <danghao.cntt@gmail.com>
2016-09-09 10:54:10 +0700

    first commit

--- app/_config.py
+++
@@ -1,6 +0,0 @@
-# -*- coding:utf-8 -*-
-__author__ = 'haopd'
-
-base_config= {
-    'base_url': 'http://niceteedesign.com'
-}
--- controller/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import logging
-
-__author__ = 'datpt'
-_logger = logging.getLogger(__name__)
-
-from controller import db
-from controller import url
-from controller import account
